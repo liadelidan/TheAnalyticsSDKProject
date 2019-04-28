10f656dfd00b6239391399e322694fc56ee318e2
liadelidan <elidanl@tcd.ie>
2019-04-28 22:53:23 +0300

    first commit

---
+++ AnalyticsSDKProject/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AnalyticsSDKProject
+//
+//  Created by Admin on 28/04/2019.
+//  Copyright © 2019 Devnostics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
