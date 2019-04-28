9f95633aa36e6b1234fddfd2cc0ff07911a8376e
liadelidan <elidanl@tcd.ie>
2019-04-28 19:06:46 +0300

    Initial Commit

---
+++ AnalyticsSDKProject/AnalyticsSDKProject.h
@@ -0,0 +1,19 @@
+//
+//  AnalyticsSDKProject.h
+//  AnalyticsSDKProject
+//
+//  Created by Admin on 28/04/2019.
+//  Copyright © 2019 Devnostics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnalyticsSDKProject.
+FOUNDATION_EXPORT double AnalyticsSDKProjectVersionNumber;
+
+//! Project version string for AnalyticsSDKProject.
+FOUNDATION_EXPORT const unsigned char AnalyticsSDKProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticsSDKProject/PublicHeader.h>
+
+
